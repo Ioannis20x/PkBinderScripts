ef6b942d49830b4e3370d2612b29ecc83e0f768e
Ioannis20x <ioannisantoniadis5@gmail.com>
2022-03-19 13:49:53 +0100

    master

--- Base.lua
+++ Base.lua
@@ -2,7 +2,9 @@ pk.onNovaLoggedIn(willkommen)
 
 function willkommen()
     local kill = nova.getTotalKills()
-local ov = TextOverlay:new("Test" ..kill, 0xFFFFFFFF, "Arial", 12, 500, 500, true, true, true)
+    local bo = BoxOverlay:new(600,500,100,200,0xFFFFFFFF, true,true)
+local ov = TextOverlay:new("Test " ..kill, 0xFFFFFFFF, "Arial", 12, 500, 500, true, true, true)
 pk.registerTextOverlay(ov)
+pk.registerBoxOverlay(bo)
 end
 
