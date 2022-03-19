073f09ae07aa7bcca5b773ac0a45d658bbf62fb4
Ioannis20x <ioannisantoniadis5@gmail.com>
2022-03-19 11:29:59 +0100

    master

--- Base.lua
+++ Base.lua
@@ -1,9 +1,7 @@
 pk.onNovaLoggedIn(willkommen)
-
 function willkommen()
     local kdoverlay = BoxOverlay:new(
         220.0,580.0, 200.0, 200.0, 0xFF646366, true,true
         )
         pk.registerBoxOverlay(kdoverlay)
     end
-pk.registerCommand(myCmd);
