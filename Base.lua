a1efcea0305b17b94196643b3d75ff55003714e7
Ioannis20x <ioannisantoniadis5@gmail.com>
2022-03-19 12:13:07 +0100

    master

--- Base.lua
+++ Base.lua
@@ -4,7 +4,7 @@ function willkommen()
 end
 
 boxCMD = Command:new("mybox", "/mybox", 0, "Box kommt box erscheinen", function(parts)
-    local kdoverlay = BoxOverlay:new(520.0, 720.0, 200.0, 200.0, 0xFF646366, true, true)
+    local kdoverlay = BoxOverlay:new(250.0, 520.0, 200.0, 200.0, 0xFF646366, true, true)
     pk.registerBoxOverlay(kdoverlay)
 end)
 
