fe9179ed20b6444f5d1027b969aa89a7b561695a
Ioannis20x <ioannisantoniadis5@gmail.com>
2022-03-19 13:19:45 +0100

    master

--- Base.lua
+++ Base.lua
@@ -3,7 +3,7 @@ pk.onNovaLoggedIn(willkommen)
 function willkommen()
     if isSharingEnabled == true then
         samp.addChatMessage("/togsharing ist aktiviert")
-    else 
+    elseif isSharingEnabled == false then
         samp.addChatMessage("DU HS")
     end
 end
