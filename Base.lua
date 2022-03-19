136de657e714f82fcc858e10b3b5aa4b2bf5d560
Ioannis20x <ioannisantoniadis5@gmail.com>
2022-03-19 19:25:52 +0100

    master

--- Base.lua
+++ Base.lua
@@ -7,7 +7,7 @@ end
 
 
 function startoverlays()
-    local bo = BoxOverlay:new(225, 400,150,125,0x78787874, true,true)
+    local bo = BoxOverlay:new(225, 700,150,125,0x78787874, true,true)
     local kills = TextOverlay:new("Kills: " ..nova.getTotalKills(), 0xFFFFFFFF, "Calibri", 14, 363, 750, true, true, true)
     local deaths  = TextOverlay:new("Tote: "..nova.getTotalDeaths(),0xFFFFFFFF, "Calibri",14,363,731,true,true,true)
     local streak = TextOverlay:new("Streak: "..nova.getKillstreak(),0xFFFFFFFF, "Calibri",14,363,701,true,true,true)
