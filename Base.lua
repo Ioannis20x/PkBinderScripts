989793fb9a838374e73793cfce40803484a31ef7
Ioannis20x <ioannisantoniadis5@gmail.com>
2022-03-19 13:41:14 +0100

    master

--- Base.lua
+++ Base.lua
@@ -1,17 +1,6 @@
---pk.onNovaLoggedIn(willkommen)
+pk.onNovaLoggedIn(willkommen)
 
 function willkommen()
-    local dialogId = samp.getNewDialogId()
-    samp.showDialog(dialogId, 1, "Titel", "Eingabe:", "Ok", "Abbrechen", function(response, listItem, text)
-        samp.addChatMessage("Die Eingabe ist: " .. text)
-        return false
-    end)
+    samp.addChatMessage(nova.getFactionById(1))
 end
 
-myCMD = Command:new("dialog", "/dialog", 0, "zeigen dialog", 
-function(parts)
-f = Faction.GetId(5)
-samp.addChatMessage( "Test" ..f)
-end)
-
-pk.registerCommand(myCMD)
