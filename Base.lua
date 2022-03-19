44d46986f76d49715986f284d07fd42cbc35e199
Ioannis20x <ioannisantoniadis5@gmail.com>
2022-03-19 11:06:21 +0100

    master

--- Base.lua
+++ Base.lua
@@ -1,11 +1,11 @@
-pk.onNovaLoggedIn(willkommen)
-function willkommen()
+pk.onNovaLoggedIn(
+function (parts)
         local kdoverlay = BoxOverlay:new(
             220.0,580.0, 200.0, 200.0, 0xFF646366, true,true
             )
             pk.registerBoxOverlay(kdoverlay)
         end
-
+    )
 myCmd = Command:new(
     'mymoney', '/mymoney',0, 'Eigenes Geld in Chat',
     function (parts)
